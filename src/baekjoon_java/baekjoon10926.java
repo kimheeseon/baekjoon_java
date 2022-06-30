4c1dd7404d95014e9c67f4d8cbc4096171a1b9a1
kimheeseon <gmltjs418@naver.com>
2022-06-30 16:20:40 +0900

    백준 10926번

---
+++ src/baekjoon_java/baekjoon10926.java
@@ -0,0 +1,14 @@
+package baekjoon_java;
+
+import java.util.Scanner;
+
+public class baekjoon10926 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String id = scanner.next();
+		System.out.println(id + "??!");
+		
+		scanner.close();
+	}	
+}

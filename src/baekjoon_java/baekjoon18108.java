1666be2d50a5bf60fa0faae7b85fe62124e0fcdc
kimheeseon <gmltjs418@naver.com>
2022-06-30 16:29:20 +0900

    백준 18108번

---
+++ src/baekjoon_java/baekjoon18108.java
@@ -0,0 +1,13 @@
+package baekjoon_java;
+import java.util.Scanner;
+
+public class baekjoon18108 {
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int year = scanner.nextInt();
+		System.out.println(year - 543);
+		
+		scanner.close();
+	}	
+}
